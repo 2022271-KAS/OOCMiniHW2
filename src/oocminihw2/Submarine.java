836b116058ed3e81d4309c6d11bd5d644c587ac6
2022271-KAS <2022271@student.cct.ie>
2023-10-15 20:31:52 +0100

    Codded and commented Chopper and Submarine classes.

---
+++ src/oocminihw2/Submarine.java
@@ -0,0 +1,24 @@
+package oocminihw2;
+
+public class Submarine extends Vehicle implements Sailable{
+
+    //Implemented the Sailable interface's methods. 
+    //Not coding the methods for the moment so just returning 0/null.
+    @Override
+    public void hoistSail() {
+    }
+
+    @Override
+    public void lowerSail() {
+    }
+
+    @Override
+    public boolean isSailHoisted() {
+        return false;
+    }
+
+    @Override
+    public void landHo() {
+    }
+    
+}
