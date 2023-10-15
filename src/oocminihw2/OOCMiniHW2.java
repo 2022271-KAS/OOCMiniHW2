979804983c05059c17a8c1149f374165e60703d9
2022271-KAS <2022271@student.cct.ie>
2023-10-15 20:37:59 +0100

    Instantiated all classes.

---
+++ src/oocminihw2/OOCMiniHW2.java
@@ -0,0 +1,21 @@
+package oocminihw2;
+
+public class OOCMiniHW2 {
+
+    public static void main(String[] args) {
+        
+        //It creates a Truck. 
+        Truck monsterTruck = new Truck();
+        System.out.println(monsterTruck);
+        
+        //It creates a Chopper.
+        Chopper choppa = new Chopper();
+        System.out.println(choppa);
+        
+        //It creates a Submarine.
+        Submarine sub = new Submarine();
+        System.out.println(sub);
+        
+    }
+    
+}
